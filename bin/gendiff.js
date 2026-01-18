8145898f99f2f4e1f179a7a8430ca9c55c9698c7
grem <mes510@yandex.ru>
2026-01-18 15:40:40 +0300

    add shebang to gendiff, remove options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,9 @@
+#!/usr/bin/env node
 import { program } from 'commander';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'display help for command');
 
-program.parse();
+program.parse(process.argv);
