d37edcb7244ef32183c463abf37c0d500e88c067
grem <mes510@yandex.ru>
2026-01-19 15:00:52 +0300

    add readFileSync and action in gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import { readFileSync } from 'node:fs';
 
 program
   .name('gendiff')
@@ -7,5 +8,9 @@ program
   .version('1.0.0')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
+  .action((file1, file2) => {
+    console.log('file1:' + readFileSync(file1));
+    console.log('file2:' + readFileSync(file2));
+  })
 
 program.parse(process.argv);
