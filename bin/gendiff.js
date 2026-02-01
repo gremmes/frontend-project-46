49f300ad45a258694ed283bb9e8376199c548c90
grem <mes510@yandex.ru>
2026-02-01 18:59:28 +0300

    moved usage of node functions from parseDataFromFile.js to gendiff.js; renamed parseDataFromFile.js to parseFromFile.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import parseDataFromFile from './parseDataFromFile.js';
+import { readFileSync } from 'node:fs';
+import { cwd } from 'node:process';
+import { resolve } from 'node:path';
+import parseFromFile from './parseFromFile.js';
 
 program
   .name('gendiff')
@@ -9,8 +12,15 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(parseDataFromFile(filepath1));
-    console.log(parseDataFromFile(filepath2));
+    const currentDirectory = cwd();
+
+    const normolizedFilePath1 = resolve(currentDirectory, filepath1);
+    const fileContent1 = readFileSync(normolizedFilePath1, 'utf-8');
+    console.log(parseFromFile(fileContent1));
+
+    const normolizedFilePath2 = resolve(currentDirectory, filepath2);
+    const fileContent2 = readFileSync(normolizedFilePath2, 'utf-8');
+    console.log(parseFromFile(fileContent2));
   })
 
 program.parse(process.argv);
---
+++ bin/parseFromFile.js
@@ -0,0 +1,1 @@
+export default (fileContent) => JSON.parse(fileContent);
