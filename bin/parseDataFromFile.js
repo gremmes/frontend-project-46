a63f10fc85a1a511e766b8b7c911d292e328e51c
grem <mes510@yandex.ru>
2026-02-01 18:34:39 +0300

    added processing of relative paths in parseDataFromFilePath.js

--- bin/parseDataFromFile.js
+++ bin/parseDataFromFile.js
@@ -1,7 +1,11 @@
 import { readFileSync } from 'node:fs';
+import { cwd } from 'node:process';
+import { resolve } from 'node:path';
 
 const parseDataFromFile = (filepath) => {
-  const fileContent = readFileSync(filepath, 'utf-8');
+  const currentDirectory = cwd();
+  const normolizedFilePath = resolve(currentDirectory, filepath);
+  const fileContent = readFileSync(normolizedFilePath, 'utf-8');
   return JSON.parse(fileContent);
 }
 
